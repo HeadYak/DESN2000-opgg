a3dcb792b59d81ed13d2cfa18938aaec0eeb695e
z5256265 <z5256265@cse.unsw.edu.au>
2020-03-31 21:20:40 +1100

    can search

--- static/index.js
+++ static/index.js
@@ -1,10 +1,9 @@
+import summoner from './summoner.js';
+
+
+
 const apikey = 'RGAPI-098d1c31-f601-45f7-96f5-20887a7b5255'
 document.getElementById('searchbtn').addEventListener('click', () => {
-    const options = {
-        method: 'GET'
-    }
-    console.log('hello')
-    fetch(`https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Skillfullism?api_key=${apikey}`, options)
-    .then(res => console.log(res.json()))
+    summoner(apikey)
     
   });

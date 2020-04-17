bfbc5c1a2d9c70dfca702ed3d5373d02e5a2a247
z5256265 <z5256265@cse.unsw.edu.au>
2020-04-17 14:18:30 +1000

    compare 2 summoners

--- static/summoner.js
+++ static/summoner.js
@@ -4,15 +4,16 @@ import summonerd from './sumdata.js'
 const apikey = 'RGAPI-098d1c31-f601-45f7-96f5-20887a7b5255'
 
 let summonerimg = document.createElement('img')
+let summonerimg1 = document.createElement('img')
 document.getElementById('searchbtn').addEventListener('click', () => {
     if ( document.getElementById('search').value == '') {
       window.alert('Please enter summoner name')
     }
     else {
-      summoner(apikey, summonerimg, summonerd)
+      summoner(apikey, summonerimg, summonerimg1, summonerd)
     }
 });
-function summoner(apikey, summonerimg, summonerd) {
+function summoner(apikey, summonerimg, summonerimg1, summonerd) {
    
     let content = document.getElementById('content')
     
@@ -32,11 +33,11 @@ function summoner(apikey, summonerimg, summonerd) {
         let summonername = document.getElementById('summonername')
         summonername.innerText = data.name + ' ' + 'Level: ' + data.summonerLevel
         content.insertBefore(summonerimg, content.children[1])
-        let acctid = data.accountId
-        if (summonerimg == null) {
-            alert(null)
-        }
+        
+        
         summonerimg.src = `https://opgg-static.akamaized.net/images/profile_icons/profileIcon${data.profileIconId}.jpg?image=q_auto&v=1518361200`
+        summonerimg.width = 150
+        summonerimg.length = 150
         let summonerid = data.id
         fetch(`https://cors-anywhere.herokuapp.com/https://oc1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerid}?api_key=${apikey}`)
         .then(res => (res.json()))
@@ -74,6 +75,59 @@ function summoner(apikey, summonerimg, summonerd) {
             }
         })*/
     })
+    if ( document.getElementById('search1').value != '') {
+        const summoner1 = document.getElementById('search1').value 
+        let content1 = document.getElementById('content1')
+        fetch(`https://cors-anywhere.herokuapp.com/https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner1}?api_key=${apikey}`, options)
+        .then(res => (res.json()))
+        .then(data => {
+            let summonername = document.getElementById('summonername1')
+            summonername.innerText = data.name + ' ' + 'Level: ' + data.summonerLevel
+            content1.insertBefore(summonerimg1, content1.children[1])
+            summonerimg1.src = `https://opgg-static.akamaized.net/images/profile_icons/profileIcon${data.profileIconId}.jpg?image=q_auto&v=1518361200`
+            summonerimg1.width = 150
+            summonerimg1.length = 150
+            let summonerid1 = data.id
+            fetch(`https://cors-anywhere.herokuapp.com/https://oc1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerid1}?api_key=${apikey}`)
+            .then(res => (res.json()))
+            .then(league => {
+                //console.log(league)
+                let queue = document.getElementById('queue1')
+                queue.innerText = ranked[league[0].queueType] 
+                document.getElementById('rank1').innerText =  league[0].tier + ' ' + league[0].rank + ' ' + league[0].leaguePoints + ' LP'
+                document.getElementById('WL1').innerText = 'Wins: ' + league[0].wins + ' Losses: ' + league[0].losses 
+    
+            })
+        })
+        document.getElementById('search1').value = ''
+        for (var record of summonerd) {
+            if (record['player'] == summoner1) {
+                document.getElementById('champ11').src = record['champ1']
+                document.getElementById('cs11').innerText = record['cs1']
+                document.getElementById('kda11').innerText = record['kda1']
+                document.getElementById('wr11').innerText = record['wr1']
+                document.getElementById('pl11').innerText = record['pl1']
+    
+                document.getElementById('champ21').src = record['champ2']
+                document.getElementById('cs21').innerText = record['cs2']
+                document.getElementById('kda21').innerText = record['kda2']
+                document.getElementById('wr21').innerText = record['wr2']
+                document.getElementById('pl21').innerText = record['pl2']
+    
+                document.getElementById('champ31').src = record['champ3']
+                document.getElementById('cs31').innerText = record['cs3']
+                document.getElementById('kda31').innerText = record['kda3']
+                document.getElementById('wr31').innerText = record['wr3']
+                document.getElementById('pl31').innerText = record['pl3']
+    
+                document.getElementById('recent1').innerText = 'W' + record['win'] + ' ' + 'L' + record['lose'] + '      ' + 'KDA: ' + record['kdar'] + '   KP: ' + record['kp']
+                document.getElementById('recentg1').style.display = 'block'
+            }
+        }
+        
+        
+    }
+    
     console.log(summoner)
     for (var record of summonerd) {
         if (record['player'] == summoner) {
@@ -94,9 +148,12 @@ function summoner(apikey, summonerimg, summonerd) {
             document.getElementById('kda3').innerText = record['kda3']
             document.getElementById('wr3').innerText = record['wr3']
             document.getElementById('pl3').innerText = record['pl3']
-            
+
+            document.getElementById('recent').innerText = 'W' + record['win'] + ' ' + 'L' + record['lose'] + '      ' + 'KDA: ' + record['kdar'] + '   KP: ' + record['kp']
+            document.getElementById('recentg').style.display = 'block'
         }
     }
+    
         
         
     
